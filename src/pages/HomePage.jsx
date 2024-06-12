515f5e4818f4fcb2b82c169b8a68fa03212ee48a
Luis <67067808+LuisLamiral8@users.noreply.github.com>
2024-06-12 13:32:57 -0300

    Large landing advances

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2128,8 +2128,411 @@ const HomePage = ({ lang }) => {
           src="/homePage/roadmap/mobileRoadmap.webp"
           alt=""
         />
+        <div className={styles.roadmapFadeUp}></div>
+        <div className={styles.roadmapFadeDown}></div>
       </section>
-      <footer></footer>
+      <section className={styles.team}>
+        <h3>{lang.team.title}</h3>
+        <div className={styles.teamFadeUp}></div>
+        <div className={styles.teamFadeDown}></div>
+        <div className={styles.teamMain}>
+          <div className={styles.teamCol}>
+            <div className={styles.teamItem}>
+              <img
+                className={styles.teamItemImage}
+                src="/homePage/team/dbrylkin.webp"
+                alt=""
+              />
+              <div>
+                <div className={styles.teamItemText}>
+                  <h5>Dimitri Brylkin</h5>
+                  <p>{lang.team.ceo}</p>
+                </div>
+              </div>
+            </div>
+            <div className={styles.teamItem}>
+              <img
+                className={styles.teamItemImage}
+                src="/homePage/team/erodriguez.webp"
+                alt=""
+              />
+              <div>
+                <div className={styles.teamItemText}>
+                  <h5>Elian Rodriguez</h5>
+                  <p>{lang.team.programmer}</p>
+                </div>
+              </div>
+            </div>
+            <div className={styles.teamItem}>
+              <img
+                className={styles.teamItemImage}
+                src="/homePage/team/lrodriguez.webp"
+                alt=""
+              />
+              <div>
+                <div className={styles.teamItemText}>
+                  <h5>Lucas Rodriguez</h5>
+                  <p>{lang.team.programmer}</p>
+                </div>
+              </div>
+            </div>
+            <div className={styles.teamItem}>
+              <img
+                className={styles.teamItemImage}
+                src="/homePage/team/francisco.webp"
+                alt=""
+              />
+              <div>
+                <div className={styles.teamItemText}>
+                  <h5>Francisco</h5>
+                  <p>{lang.team.programmer}</p>
+                </div>
+              </div>
+            </div>
+            <div className={styles.teamItem}>
+              <img
+                className={styles.teamItemImage}
+                src="/homePage/team/llamiral.webp"
+                alt=""
+              />
+              <div>
+                <div className={styles.teamItemText}>
+                  <h5>Luis Lamiral</h5>
+                  <p>{lang.team.webDeveloper}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.teamCol}>
+            <div className={styles.teamItem}>
+              <img
+                className={styles.teamItemImage}
+                src="/homePage/team/richard.webp"
+                alt=""
+              />
+              <div>
+                <div className={styles.teamItemText}>
+                  <h5>Richard</h5>
+                  <p>{lang.team.threeDDesigner}</p>
+                </div>
+              </div>
+            </div>
+            <div className={styles.teamItem}>
+              <img
+                className={styles.teamItemImage}
+                src="/homePage/team/jhon.webp"
+                alt=""
+              />
+              <div>
+                <div className={styles.teamItemText}>
+                  <h5>Jhon</h5>
+                  <p>{lang.team.threeDDesigner}</p>
+                </div>
+              </div>
+            </div>
+            <div className={styles.teamItem}>
+              <img
+                className={styles.teamItemImage}
+                src="/homePage/team/amartinez.webp"
+                alt=""
+              />
+              <div>
+                <div className={styles.teamItemText}>
+                  <h5>Adrian Martinez</h5>
+                  <p>{lang.team.worldDesigner}</p>
+                </div>
+              </div>
+            </div>
+            <div className={styles.teamItem}>
+              <img
+                className={styles.teamItemImage}
+                src="/homePage/team/cristobal.webp"
+                alt=""
+              />
+              <div>
+                <div className={styles.teamItemText}>
+                  <h5>Cristobal</h5>
+                  {/* <p>{lang.team.programmer}</p> */}
+                  <p>...</p>
+                </div>
+              </div>
+            </div>
+            <div className={styles.teamItem}>
+              <img
+                className={styles.teamItemImage}
+                src="/homePage/team/luis.webp"
+                alt=""
+              />
+              <div>
+                <div className={styles.teamItemText}>
+                  <h5>Luis</h5>
+                  <p>{lang.team.programmer}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.teamMobileMain}>
+          <div className={styles.teamCol}>
+            <div className={styles.teamItem}>
+              <img
+                className={styles.teamItemImage}
+                src="/homePage/team/dbrylkin.webp"
+                alt=""
+              />
+              <div>
+                <div className={styles.teamItemText}>
+                  <h5>Dimitri Brylkin</h5>
+                  <p>{lang.team.ceo}</p>
+                </div>
+              </div>
+            </div>
+            <div className={styles.teamItem}>
+              <img
+                className={styles.teamItemImage}
+                src="/homePage/team/erodriguez.webp"
+                alt=""
+              />
+              <div>
+                <div className={styles.teamItemText}>
+                  <h5>Elian Rodriguez</h5>
+                  <p>{lang.team.programmer}</p>
+                </div>
+              </div>
+            </div>
+            <div className={styles.teamItem}>
+              <img
+                className={styles.teamItemImage}
+                src="/homePage/team/lrodriguez.webp"
+                alt=""
+              />
+              <div>
+                <div className={styles.teamItemText}>
+                  <h5>Lucas Rodriguez</h5>
+                  <p>{lang.team.programmer}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.teamCol}>
+            <div className={styles.teamItem}>
+              <img
+                className={styles.teamItemImage}
+                src="/homePage/team/francisco.webp"
+                alt=""
+              />
+              <div>
+                <div className={styles.teamItemText}>
+                  <h5>Francisco</h5>
+                  <p>{lang.team.programmer}</p>
+                </div>
+              </div>
+            </div>
+            <div className={styles.teamItem}>
+              <img
+                className={styles.teamItemImage}
+                src="/homePage/team/llamiral.webp"
+                alt=""
+              />
+              <div>
+                <div className={styles.teamItemText}>
+                  <h5>Luis Lamiral</h5>
+                  <p>{lang.team.webDeveloper}</p>
+                </div>
+              </div>
+            </div>
+
+            <div className={styles.teamItem}>
+              <img
+                className={styles.teamItemImage}
+                src="/homePage/team/richard.webp"
+                alt=""
+              />
+              <div>
+                <div className={styles.teamItemText}>
+                  <h5>Richard</h5>
+                  <p>{lang.team.threeDDesigner}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.teamCol}>
+            <div className={styles.teamItem}>
+              <img
+                className={styles.teamItemImage}
+                src="/homePage/team/jhon.webp"
+                alt=""
+              />
+              <div>
+                <div className={styles.teamItemText}>
+                  <h5>Jhon</h5>
+                  <p>{lang.team.threeDDesigner}</p>
+                </div>
+              </div>
+            </div>
+            <div className={styles.teamItem}>
+              <img
+                className={styles.teamItemImage}
+                src="/homePage/team/amartinez.webp"
+                alt=""
+              />
+              <div>
+                <div className={styles.teamItemText}>
+                  <h5>Adrian Martinez</h5>
+                  <p>{lang.team.worldDesigner}</p>
+                </div>
+              </div>
+            </div>
+            <div className={styles.teamItem}>
+              <img
+                className={styles.teamItemImage}
+                src="/homePage/team/cristobal.webp"
+                alt=""
+              />
+              <div>
+                <div className={styles.teamItemText}>
+                  <h5>Cristobal</h5>
+                  {/* <p>{lang.team.programmer}</p> */}
+                  <p>...</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.teamCol}>
+            <div className={styles.teamItem}>
+              <img
+                className={styles.teamItemImage}
+                src="/homePage/team/luis.webp"
+                alt=""
+              />
+              <div>
+                <div className={styles.teamItemText}>
+                  <h5>Luis</h5>
+                  <p>{lang.team.programmer}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className={styles.getReady}>
+        <div className={styles.getReadyFadeUp}></div>
+        <div className={styles.getReadyFadeDown}>
+          <img src="/homePage/getReady/clouds.webp" alt="" />
+        </div>
+        <img
+          className={styles.getReadyAxolotl}
+          src="/homePage/getReady/axolotl.webp"
+          alt=""
+        />
+        <img
+          className={styles.getReadyEmbear}
+          src="/homePage/getReady/embear.webp"
+          alt=""
+        />
+        <h4>{lang.getReady.title}</h4>
+        <p>{lang.getReady.text}</p>
+        <label>{lang.getReady.label}</label>
+        <div className={styles.getReadyInput}>
+          <input type="text" placeholder={lang.getReady.placeholder} />
+          <button>{lang.getReady.signIn}</button>
+        </div>
+
+        <div className={styles.getReadySocial}>
+          <h5>{lang.getReady.joinBattle}</h5>
+          <div className={styles.getReadySocialMain}>
+            <button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="1em"
+                height="1em"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="#ffff"
+                  d="M19.27 5.33C17.94 4.71 16.5 4.26 15 4a.09.09 0 0 0-.07.03c-.18.33-.39.76-.53 1.09a16.09 16.09 0 0 0-4.8 0c-.14-.34-.35-.76-.54-1.09c-.01-.02-.04-.03-.07-.03c-1.5.26-2.93.71-4.27 1.33c-.01 0-.02.01-.03.02c-2.72 4.07-3.47 8.03-3.1 11.95c0 .02.01.04.03.05c1.8 1.32 3.53 2.12 5.24 2.65c.03.01.06 0 .07-.02c.4-.55.76-1.13 1.07-1.74c.02-.04 0-.08-.04-.09c-.57-.22-1.11-.48-1.64-.78c-.04-.02-.04-.08-.01-.11c.11-.08.22-.17.33-.25c.02-.02.05-.02.07-.01c3.44 1.57 7.15 1.57 10.55 0c.02-.01.05-.01.07.01c.11.09.22.17.33.26c.04.03.04.09-.01.11c-.52.31-1.07.56-1.64.78c-.04.01-.05.06-.04.09c.32.61.68 1.19 1.07 1.74c.03.01.06.02.09.01c1.72-.53 3.45-1.33 5.25-2.65c.02-.01.03-.03.03-.05c.44-4.53-.73-8.46-3.1-11.95c-.01-.01-.02-.02-.04-.02M8.52 14.91c-1.03 0-1.89-.95-1.89-2.12s.84-2.12 1.89-2.12c1.06 0 1.9.96 1.89 2.12c0 1.17-.84 2.12-1.89 2.12m6.97 0c-1.03 0-1.89-.95-1.89-2.12s.84-2.12 1.89-2.12c1.06 0 1.9.96 1.89 2.12c0 1.17-.83 2.12-1.89 2.12"
+                />
+              </svg>
+            </button>
+            <button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="1em"
+                height="1em"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="#ffff"
+                  d="M11.64 5.93h1.43v4.28h-1.43m3.93-4.28H17v4.28h-1.43M7 2L3.43 5.57v12.86h4.28V22l3.58-3.57h2.85L20.57 12V2m-1.43 9.29l-2.85 2.85h-2.86l-2.5 2.5v-2.5H7.71V3.43h11.43Z"
+                />
+              </svg>
+            </button>
+            <button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="1em"
+                height="1em"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="#ffff"
+                  d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4zm9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3"
+                />
+              </svg>
+            </button>
+            <button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="1em"
+                height="1em"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="#ffff"
+                  d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95c5.05-.5 9-4.76 9-9.95"
+                />
+              </svg>
+            </button>
+            <button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="0.88em"
+                height="1em"
+                viewBox="0 0 448 512"
+              >
+                <path
+                  fill="#ffff"
+                  d="M64 32C28.7 32 0 60.7 0 96v320c0 35.3 28.7 64 64 64h320c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64zm297.1 84L257.3 234.6L379.4 396h-95.6L209 298.1L123.3 396H75.8l111-126.9L69.7 116h98l67.7 89.5l78.2-89.5zm-37.8 251.6L153.4 142.9h-28.3l171.8 224.7h26.3z"
+                />
+              </svg>
+            </button>
+            <button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="1em"
+                height="1em"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="#ffff"
+                  d="m10 15l5.19-3L10 9zm11.56-7.83c.13.47.22 1.1.28 1.9c.07.8.1 1.49.1 2.09L22 12c0 2.19-.16 3.8-.44 4.83c-.25.9-.83 1.48-1.73 1.73c-.47.13-1.33.22-2.65.28c-1.3.07-2.49.1-3.59.1L12 19c-4.19 0-6.8-.16-7.83-.44c-.9-.25-1.48-.83-1.73-1.73c-.13-.47-.22-1.1-.28-1.9c-.07-.8-.1-1.49-.1-2.09L2 12c0-2.19.16-3.8.44-4.83c.25-.9.83-1.48 1.73-1.73c.47-.13 1.33-.22 2.65-.28c1.3-.07 2.49-.1 3.59-.1L12 5c4.19 0 6.8.16 7.83.44c.9.25 1.48.83 1.73 1.73"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </section>
+      <footer className={styles.footer}>
+        <div>
+          <img src="/homePage/footer/zelcar.png" alt="" />
+          <img src="/homePage/footer/secret.png" alt="" />
+        </div>
+        <div>
+          <a href="#">{lang.footer.termsCondition}</a>
+          <a href="#">{lang.footer.privacyPolicy}</a>
+          <a href="#">{lang.footer.cookiesPolicy}</a>
+        </div>
+      </footer>
     </main>
   );
 };
